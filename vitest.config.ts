213dd1cbff01e1d39d93cbb140d5ca9773b5a5c3
Erwin Villejo <erwinvillejo@gmail.com>
2023-12-02 18:00:46 +0800

    enable coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      enabled: true,
+      reporter: ['html'],
+    },
+    reporters: ['default', 'html'],
   },
 })
